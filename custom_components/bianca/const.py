d26b165fbd2ff497b8d0d23232fc71e6c5c0a493
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-29 22:37:15 +0300

    Update const.py

--- custom_components/bianca/const.py
+++ custom_components/bianca/const.py
@@ -2,13 +2,13 @@
 
 DOMAIN = "bianca"
 CONF_INTEGRATION_TITLE = "Bianca"
-DEFAULT_SCAN_INTERVAL = 25
+DEFAULT_SCAN_INTERVAL = 45
 
 CONF_IP_ADDRESS = "ip_address"
 
 API_ENDPOINT = "http://{}/http-read.json?encrypted=0"
 
-PLATFORMS = ["sensor"]
+PLATFORMS = ["sensor", "binary_sensor"]
 
 # MachMd (machine mode) mapping
 MACHMD_MAP = {
