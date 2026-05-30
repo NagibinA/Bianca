8dc7cda148ca1aec6e9148657d0c0717f5592320
NagibinA <91381276+NagibinA@users.noreply.github.com>
2026-05-30 18:56:55 +0300

    Update binary_sensor.py

--- custom_components/bianca/binary_sensor.py
+++ custom_components/bianca/binary_sensor.py
@@ -51,7 +51,7 @@ class BiancaAvailableBinarySensor(BinarySensorEntity):
         self._entry = entry
         self._ip_address = ip_address
         self.entity_id = "binary_sensor.bianca_available"
-        self._attr_name = "Bianca Available"
+        self._attr_name = "Bianca Доступность"
         self._attr_unique_id = f"{entry.entry_id}_available"
         self._attr_icon = "mdi:network"
         self._attr_should_poll = False
